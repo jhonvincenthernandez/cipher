c6cb0d38dcf748061328b5729eac7feffe43732f
jhonvincenthernandez <jhonvincenthernandez9@gamil.com>
2026-09-18 20:33:06 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'caesar-cipher',
+  webDir: 'dist'
+};
+
+export default config;
